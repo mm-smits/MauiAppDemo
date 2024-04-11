dbf4cf0d79a0d0c119780ec5bc8c347fb1ea02f6
mm-smits <mm.smitsolanki@gmail.com>
2024-04-11 17:43:43 +0530

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterBtn2.Text = "hi";
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+            SemanticScreenReader.Announce(CounterBtn2.Text);
+        }
+    }
+
+}
